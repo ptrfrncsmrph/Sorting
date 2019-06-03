47f26ef1aef027e322c37cc6c30b48a8b284ce49
Peter Murphy <peter.francis.murphy@gmail.com>
2019-06-03 17:01:47 -0400

    Clean up bubble_sort and selection_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,40 +1,25 @@
-# TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
-    # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
-        # TO-DO: swap
-        temp = arr[cur_index]
-        arr[cur_index] = arr[smallest_index]
-        arr[smallest_index] = temp
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
-
-# TO-DO:  implement the Bubble Sort function below
 
 
 def bubble_sort(arr):
     swaps = 0
     for i in range(0, len(arr) - 1):
         if arr[i] > arr[i + 1]:
-            temp = arr[i]
-            arr[i] = arr[i + 1]
-            arr[i + 1] = temp
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
             swaps += 1
     if swaps == 0:
         return arr
     else:
         return bubble_sort(arr)
-
-
-bubble_sort([1, 9, 5, 2])
-print(bubble_sort([1, 9, 5, 2]))
 
 
 # STRETCH: implement the Count Sort function below
